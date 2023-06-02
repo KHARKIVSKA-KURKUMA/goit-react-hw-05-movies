c2ac2c38a121225a6cf393c82a40c5804e55d73a
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-06-02 19:53:53 +0300

    add react lazy load

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,31 @@
-import Cast from 'components/Cast/Cast';
-import Home from 'components/Home/Home';
-import Layout from 'components/Layout/Layout';
-import MovieDetails from 'components/MovieDetails/MovieDetails';
-import Movies from 'components/Movies/Movies';
-import Reviews from 'components/Reviews/Reviews';
 import { Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
 import { ToastContainer } from 'react-toastify';
+/* -------------------------------------------------------------------------- */
+const Layout = lazy(() => import('components/Layout/Layout'));
+const Home = lazy(() => import('components/Home/Home'));
+const Movies = lazy(() => import('components/Movies/Movies'));
+const MovieDetails = lazy(() => import('components/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 const App = () => {
   return (
     <>
       <ToastContainer autoClose={1500} theme="colored" />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId/" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId/" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 };
-
 export default App;
